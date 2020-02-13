a3d2ffb4ee78cb61da80e08b3fcac35f2f1d188c
Mike Raysor <michaelraysor1@gmail.com>
2020-02-12 23:03:35 -0500

    updated Index.js with Router Paths, and Navigation with imports and Router Links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-jj
+
+const Apple = () => {
+    return (
+        <div className="App">
+            <nav>
+                <div className="nav-links" />
+                </nav>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Contact" component={Contact} />
+            
+        </div>
+    );
+}; 
+
+    
+
+
+ReactDOM.render(
+    <render>
+       <App />
+        </render>, 
+        document.getElementById('root'));
