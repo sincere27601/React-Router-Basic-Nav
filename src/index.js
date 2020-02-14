f6b6a533ffdb848313bf3f96b0b16b121485feb9
Mike Raysor <michaelraysor1@gmail.com>
2020-02-13 21:20:43 -0500

    updated import React useState in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
